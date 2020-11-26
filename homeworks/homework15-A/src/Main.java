4c2b6c510701e1e8875da1d2fba0ffc1d23c54a1
ezdes <71791856+ezdes@users.noreply.github.com>
2020-11-27 01:05:31 +0600

    Update Main.java

--- homeworks/homework15-A/src/Main.java
+++ homeworks/homework15-A/src/Main.java
@@ -16,6 +16,6 @@ public class Main {
         Person[] people = {person1, person2, person3, person4, person5, person6, person7, person8, person9, person10};
 
         for (Person p : people)
-            p.displayPeople(people);
+            p.displayPeople();
     }
 }
